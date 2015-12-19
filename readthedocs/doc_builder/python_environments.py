98d128fe4f89dfe4c3fca2f1393b413209ae7696
Eric Holscher <eric@ericholscher.com>
2015-12-18 16:19:08 -0800

    Upgrade docutils as well

--- readthedocs/doc_builder/python_environments.py
+++ readthedocs/doc_builder/python_environments.py
@@ -101,7 +101,7 @@ class Virtualenv(PythonEnvironment):
             'sphinx==1.3.3',
             'Pygments==2.0.2',
             'setuptools==18.6.1',
-            'docutils==0.11',
+            'docutils==0.12',
             'mkdocs==0.14.0',
             'mock==1.0.1',
             'pillow==2.6.1',
@@ -189,7 +189,7 @@ class Conda(PythonEnvironment):
         requirements = [
             'sphinx==1.3.1',
             'Pygments==2.0.2',
-            'docutils==0.11',
+            'docutils==0.12',
             # 'mkdocs==0.14.0',
             'mock==1.0.1',
             'pillow==3.0.0',
