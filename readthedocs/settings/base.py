9239a5b6563f77b1a8f909de7147326cf63f35d0
Eric Holscher <eric@ericholscher.com>
2017-12-06 21:54:21 -0800

    Upgrade search app to Elastic Search 5.x

--- readthedocs/settings/base.py
+++ readthedocs/settings/base.py
@@ -122,6 +122,7 @@ class CommunityBaseSettings(Settings):
             apps.append('django_countries')
             apps.append('readthedocsext.donate')
             apps.append('readthedocsext.embed')
+            apps.append('readthedocsext.search')
         return apps
 
     @property
