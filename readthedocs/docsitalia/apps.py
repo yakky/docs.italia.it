454d2bd3b8bd195c54a2d49bcec84a3f7e4e0325
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2018-04-10 16:12:03 +0200

    docsitalia: fixup docsitalia AppConfig

--- readthedocs/docsitalia/apps.py
+++ readthedocs/docsitalia/apps.py
@@ -3,8 +3,9 @@ from __future__ import absolute_import, unicode_literals
 from django.apps import AppConfig
 
 
-class DocsitaliaConfig(AppConfig):
-    name = 'docsitalia'
+class DocsItaliaConfig(AppConfig):
+    name = 'readthedocs.docsitalia'
+    verbose_name = 'Docs Italia'
 
     def ready(self):
         import readthedocs.docsitalia.signals # noqa
