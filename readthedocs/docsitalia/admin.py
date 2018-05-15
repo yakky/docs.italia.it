9d371aaffbef62f061e535eb18fe64cf07379407
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2018-05-15 14:42:05 +0200

    docsitalia: add featured field to publisher project
    So that they can be flagged and filtered in homepage

--- readthedocs/docsitalia/admin.py
+++ readthedocs/docsitalia/admin.py
@@ -5,5 +5,13 @@ from django.contrib import admin
 
 from .models import Publisher, PublisherProject
 
+
+class PublisherProjectAdmin(admin.ModelAdmin):
+
+    """Admin view for :py:class:`PublisherProject`"""
+
+    list_filter = ('featured',)
+
+
 admin.site.register(Publisher)
-admin.site.register(PublisherProject)
+admin.site.register(PublisherProject, PublisherProjectAdmin)
