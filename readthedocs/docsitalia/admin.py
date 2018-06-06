3d6cff1a5db4f03c523b212d23f45cc8af8f436d
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2018-06-06 11:16:26 +0200

    docsitalia: better admin for publisher
    Make it clear what must be edited and what not.

--- readthedocs/docsitalia/admin.py
+++ readthedocs/docsitalia/admin.py
@@ -2,8 +2,30 @@
 """Admin for the docsitalia app."""
 
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Publisher, PublisherProject
+
+
+class PublisherAdmin(admin.ModelAdmin):
+
+    """Admin view for :py:class:`Publisher`"""
+
+    readonly_fields = ('metadata', 'projects_metadata',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'slug', 'active'),
+        }),
+        (_('Advanced Settings'), {
+            'classes': ('collapse',),
+            'fields': (
+                'metadata',
+                'projects_metadata',
+                'config_repo_name',
+                'remote_organization',
+            )
+        }),
+    )
 
 
 class PublisherProjectAdmin(admin.ModelAdmin):
@@ -13,5 +35,5 @@ class PublisherProjectAdmin(admin.ModelAdmin):
     list_filter = ('featured',)
 
 
-admin.site.register(Publisher)
+admin.site.register(Publisher, PublisherAdmin)
 admin.site.register(PublisherProject, PublisherProjectAdmin)
