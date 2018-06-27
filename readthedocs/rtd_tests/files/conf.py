4adace61463330ec6eeda2cc5bc9957d565cb68c
Davide Porrovecchio <pdavide@gmail.com>
2018-06-27 17:21:40 +0200

    Update conf.py template
    - support optional conf.py
    - add docs-italia-theme default settings
    - remove template extensibility related code
    - update some comments
    - update test conf.py

--- readthedocs/rtd_tests/files/conf.py
+++ readthedocs/rtd_tests/files/conf.py
@@ -4,14 +4,9 @@ from __future__ import division, print_function, unicode_literals
 
 from datetime import datetime
 
-from recommonmark.parser import CommonMarkParser
-
 extensions = []
 templates_path = ['/tmp/sphinx-template-dir', 'templates', '_templates', '.templates']
-source_suffix = ['.rst', '.md']
-source_parsers = {
-            '.md': CommonMarkParser,
-        }
+source_suffix = ['.rst']
 master_doc = 'index'
 project = u'Pip'
 copyright = str(datetime.now().year)
@@ -20,9 +15,9 @@ release = '0.8.1'
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 htmlhelp_basename = 'pip'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'docs-italia-theme'
 file_insertion_enabled = False
 latex_documents = [
-  ('index', 'pip.tex', u'Pip Documentation',
+  ('index', 'pip.tex', u'Pip',
    u'', 'manual'),
 ]
