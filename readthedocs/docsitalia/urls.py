f6b2f03f18e4272c77d2fa72feea0bc69709e521
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2018-07-04 11:35:14 +0200

    docsitalia: there's not such thing as manual import
    Redirect manual import and demo import urls to automatic import

--- readthedocs/docsitalia/urls.py
+++ readthedocs/docsitalia/urls.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from django.apps import apps
 from django.conf import settings
 from django.conf.urls import include, url
-from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView, TemplateView
 
 from readthedocs.constants import pattern_opts
 from rest_framework import routers
@@ -27,6 +27,12 @@ docsitalia_urls = [
     url(r'^dashboard/import/$',
         DocsItaliaImport.as_view(),
         name='projects_import'),
+    url(r'^dashboard/import/manual/$',
+        RedirectView.as_view(pattern_name='projects_import'),
+        name='projects_import_manual'),
+    url(r'^dashboard/import/manual/demo/$',
+        RedirectView.as_view(pattern_name='projects_import'),
+        name='projects_import_demo'),
 ]
 
 urlpatterns = [
--- readthedocs/rtd_tests/tests/test_privacy_urls.py
+++ readthedocs/rtd_tests/tests/test_privacy_urls.py
@@ -227,7 +227,8 @@ class PrivateProjectAdminAccessTest(PrivateProjectMixin, TestCase):
 
     response_data = {
         # Places where we 302 on success -- These delete pages should probably be 405'ing
-        '/dashboard/import/manual/demo/': {'status_code': 302},
+        '/docsitalia/dashboard/import/manual/demo/': {'status_code': 302},
+        '/docsitalia/dashboard/import/manual/': {'status_code': 302},
         '/dashboard/pip/': {'status_code': 302},
         '/dashboard/pip/subprojects/delete/sub/': {'status_code': 302},
         '/dashboard/pip/translations/delete/sub/': {'status_code': 302},
@@ -263,8 +264,8 @@ class PrivateProjectUserAccessTest(PrivateProjectMixin, TestCase):
         # Auth'd users can import projects, have no perms on pip
         '/dashboard/': {'status_code': 200},
         '/docsitalia/dashboard/import/': {'status_code': 200},
-        '/dashboard/import/manual/': {'status_code': 200},
-        '/dashboard/import/manual/demo/': {'status_code': 302},
+        '/docsitalia/dashboard/import/manual/': {'status_code': 302},
+        '/docsitalia/dashboard/import/manual/demo/': {'status_code': 302},
 
         # Unauth access redirect for non-owners
         '/dashboard/pip/': {'status_code': 302},
