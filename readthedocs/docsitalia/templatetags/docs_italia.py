3336ac16bae60a7e27313c2371b7b2407a398359
Iacopo Spalletti <i.spalletti@nephila.it>
2018-10-17 12:09:28 +0200

    Temporary fix for broken static files in subdirectories

--- readthedocs/docsitalia/templatetags/docs_italia.py
+++ readthedocs/docsitalia/templatetags/docs_italia.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 
 from django import template
+from readthedocs.core.resolver import resolve
 
 from ..models import PublisherProject
 
@@ -15,3 +16,14 @@ def get_publisher_project(slug):
         return PublisherProject.objects.get(slug=slug)
     except PublisherProject.DoesNotExist:
         return None
+
+
+@register.simple_tag(name="doc_url_patched")
+def make_document_url(project, version=None, page=''):
+    """create the full document URL and appends index.html if root"""
+    if not project:
+        return ""
+    url = resolve(project=project, version_slug=version, filename=page)
+    if url.endswith('/'):
+        url = '%sindex.html' % url
+    return url
