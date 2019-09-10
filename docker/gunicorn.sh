3b31826fcc06a13b99445dfd3ca3f7b4be810292
Paolo Melchiorre <paolo@20tab.com>
2019-09-10 12:24:31 +0200

    Fix local docker compose setup

--- docker/gunicorn.sh
+++ docker/gunicorn.sh
@@ -5,6 +5,6 @@ set -e
 docker/dirs.sh
 if [ "$1" = "collect" ]; then
   cp -a media /home/documents/
-  /virtualenv/bin/python manage.py collectstatic --no-input
+  /virtualenv/bin/python manage.py collectstatic --no-input -c
 fi
 /virtualenv/bin/gunicorn --error-logfile="-"  readthedocs.wsgi:application $*
