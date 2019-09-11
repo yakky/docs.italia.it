aea405c28fedaf51796d027f5c5e1185f49e583a
Fabio Sangiovanni <f.sangiovanni@nephila.it>
2019-09-11 11:09:04 +0200

    Add Elasticsearch provisioning to web containers. Provision Elasticsearch only if no index exists yet.

--- docker/gunicorn.sh
+++ docker/gunicorn.sh
@@ -7,4 +7,6 @@ if [ "$1" = "collect" ]; then
   cp -a media /home/documents/
   /virtualenv/bin/python manage.py collectstatic --no-input -c
 fi
+sleep 10
+/virtualenv/bin/python manage.py provision_elasticsearch --ensure-no-index
 /virtualenv/bin/gunicorn --error-logfile="-"  readthedocs.wsgi:application $*
