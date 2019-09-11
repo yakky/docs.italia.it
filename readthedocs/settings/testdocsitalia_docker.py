b580dfb0a3fd06f4b9824d884f98fe7cd33121eb
Fabio Sangiovanni <f.sangiovanni@nephila.it>
2019-09-11 15:18:57 +0200

    Run tests inside docker container.

---
+++ readthedocs/settings/testdocsitalia_docker.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+import os
+
+from .test import CommunityTestSettings
+
+
+CommunityTestSettings.load_settings(__name__)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'PREFIX': 'docs',
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+        'PASSWORD': 'postgres',
+        'NAME': 'test_docsitalia'
+    }
+}
+
+
+if not os.environ.get('DJANGO_SETTINGS_SKIP_LOCAL', False):
+    try:
+        from .test_local_settings import *  # noqa
+    except ImportError:
+        pass
