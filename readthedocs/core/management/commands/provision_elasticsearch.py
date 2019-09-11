7075376a86dd2480f4dddb4a47c0e7bb26033a9e
Fabio Sangiovanni <f.sangiovanni@nephila.it>
2019-09-11 11:09:04 +0200

    Add Elasticsearch provisioning to web containers. Provision Elasticsearch only if no index exists yet.

--- readthedocs/core/management/commands/provision_elasticsearch.py
+++ readthedocs/core/management/commands/provision_elasticsearch.py
@@ -3,8 +3,10 @@
 from __future__ import absolute_import
 import logging
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
+from elasticsearch import Elasticsearch
 from readthedocs.search.indexes import Index, PageIndex, ProjectIndex, SectionIndex
 
 log = logging.getLogger(__name__)
@@ -14,7 +16,25 @@ class Command(BaseCommand):
 
     help = __doc__
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--ensure-no-index', default=False, action='store_true',
+            help='Perform provisioning only if no index exists yet.',
+        )
+
     def handle(self, *args, **options):
+        if options['ensure_no_index'] and self._index_exists():
+            log.info(
+                'Skipping provisioning: at least one index exists already.',
+            )
+            return
+        self._provision_es()
+
+    def _index_exists(self):
+        indexes = len(Elasticsearch(settings.ES_HOSTS).indices.get_alias())
+        return bool(indexes)
+
+    def _provision_es(self):
         """Provision new ES instance"""
         index = Index()
         index_name = index.timestamped_index()
