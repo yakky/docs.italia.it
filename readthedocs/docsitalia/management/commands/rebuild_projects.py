eebe794c9b4632021d799be57ec641489c5355c8
Iacopo Spalletti <i.spalletti@nephila.digital>
2019-09-18 12:46:11 +0200

    Make rebuild_projects async (#233)
    This allows to run `rebuild_projects` async
    
    The default sync behaviour has been preserved

--- readthedocs/docsitalia/management/commands/rebuild_projects.py
+++ readthedocs/docsitalia/management/commands/rebuild_projects.py
@@ -4,11 +4,8 @@ from __future__ import absolute_import
 from django.core.management.base import BaseCommand, CommandError
 
 from readthedocs.builds.models import Build, Version
-from readthedocs.projects.tasks import UpdateDocsTaskStep
+from readthedocs.projects.tasks import UpdateDocsTask
 from readthedocs.projects.models import Project
-from readthedocs.doc_builder.config import load_yaml_config
-from readthedocs.doc_builder.environments import LocalBuildEnvironment
-from readthedocs.doc_builder.python_environments import Virtualenv
 
 
 from readthedocs.docsitalia.models import Publisher, PublisherProject
@@ -30,12 +27,22 @@ class Command(BaseCommand):
             '--document', nargs='?', type=str,
             help='A Read the docs document slug'
         )
+        parser.add_argument(
+            '--version', nargs='?', type=str,
+            help='A Read the docs version slug'
+        )
+        parser.add_argument(
+            '--async', action='store_true', default=False,
+            help='Run the rebuild tasks async'
+        )
 
     def handle(self, *args, **options):
         """handle command"""
         versions = Version.objects.all()
         publisher = options['publisher']
+        version = options['version']
         document = options['document']
+        run_async = options['async']
         if publisher:
             try:
                 projects = PublisherProject.objects.filter(
@@ -50,18 +57,23 @@ class Command(BaseCommand):
                 versions = versions.filter(project=project)
             except Project.DoesNotExist:
                 raise CommandError("Project {} doesn't exist".format(document))
+        if version:
+            try:
+                versions = versions.filter(slug=version)
+            except Project.DoesNotExist:
+                raise CommandError("Project {} doesn't exist".format(document))
         for version in versions:
-            build_env = LocalBuildEnvironment(project=version.project, version=version, build={})
-            python_env = Virtualenv(version=version, build_env=build_env)
-            config = load_yaml_config(version)
-            task = UpdateDocsTaskStep(
-                build_env=build_env, project=version.project, python_env=python_env,
-                version=version, search=True, localmedia=False, config=config
-            )
+            task = UpdateDocsTask()
             build = Build.objects.create(
                 project=version.project,
                 version=version,
                 type='html',
                 state='triggered',
             )
-            task.run(version.project.pk, version_pk=version.pk, build_pk=build.pk)
+            kwargs = dict(
+                pk=version.project.pk, version_pk=version.pk, build_pk=build.pk, search=True
+            )
+            if run_async:
+                task.apply_async(kwargs=kwargs)
+            else:
+                task.run(**kwargs)
